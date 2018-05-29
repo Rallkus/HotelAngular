bb0b08ffbe2bea18055ba1f0933a1bd173e15807
Rallkus <serhuegi@gmail.com>
2018-05-29 11:44:47 +0200

    Autocomplete done

--- frontend/modules/home/controller/home.controller.js
+++ frontend/modules/home/controller/home.controller.js
@@ -1,9 +1,17 @@
 hotel.controller('homeCtrl', function ($scope, services, hoteles) {
     $scope.filteredHoteles = [];
+    $scope.hideAutocomplete = false;
     $scope.hotel = hoteles;
     $scope.numPerPage = 6;
     $scope.currentPage = 1;
-    console.log($scope.hotel);
+    $scope.hotelNames = [];
+    $scope.autocompleteNames = [];
+    angular.forEach($scope.hotel, function(value, key) {
+      $scope.hotelNames.push(value.nombre);
+    });
+
+    var scroll = true;
+    console.log($scope.hotelNames);
 
     $scope.nextScroll = function() {
 	  var startPos = ($scope.currentPage - 1) * 3;
@@ -12,6 +20,24 @@ hotel.controller('homeCtrl', function ($scope, services, hoteles) {
     $scope.filteredHoteles.push($scope.hotel[startPos+2]);
     $scope.filteredHoteles.push($scope.hotel[startPos+3]);
     $scope.currentPage++;
-	  console.log($scope.currentPage);
 	};
+  $scope.cleanList = function() {
+  $scope.filteredHoteles = [];
+  scroll = false;
+};
+$scope.complete = function(string){
+  $scope.hideAutocomplete = false;
+  var array = [];
+  angular.forEach($scope.hotelNames, function(value, key) {
+    if(value.toLowerCase().indexOf(string.toLowerCase()) >= 0){
+      array.push(value);
+    }
+
+  });
+  $scope.autocompleteNames = array;
+}
+$scope.fillQuery = function(string){
+  $scope.query = string;
+  $scope.hideAutocomplete = true;
+}
 });

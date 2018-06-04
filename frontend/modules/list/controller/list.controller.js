9e059638fad0479b2c2eff1d1d0efdaaa59f33c5
Rallkus <serhuegi@gmail.com>
2018-06-04 12:04:37 +0200

    Maps added

--- frontend/modules/list/controller/list.controller.js
+++ frontend/modules/list/controller/list.controller.js
@@ -5,4 +5,35 @@ hotel.controller('detailsCtrl', function ($scope, services, oferta) {
 hotel.controller('listCtrl', function ($scope, services, list) {
     $scope.list = list;
     console.log(list);
+    var maker;
+    var a = [];
+    var hotel = [];
+    var i = 0;
+    function initMap() {
+      var myLatLng = {lat: 40.4167, lng: -3.70325};
+
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 6,
+        center: myLatLng
+      });
+      angular.forEach($scope.list, function(value, key) {
+        var content = value.nombre + " <br> "+value.municipio+" <br> <a href=#/list/details/"+value.id+">Details</a>";
+        var infowindow = new google.maps.InfoWindow();
+        marker = new google.maps.Marker({
+          map: map,
+          draggable: true,
+          animation: google.maps.Animation.DROP,
+          position: {lat: value.lat, lng: value.lng}
+        });
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+          return function() {
+            infowindow.setContent(content);
+            infowindow.open(map, marker);
+          }
+        })(marker, i));
+        i++;
+      });
+
+    }
+    initMap();
 });
